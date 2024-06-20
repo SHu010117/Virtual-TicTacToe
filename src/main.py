121d0b80e6e4282a4ad55e58b147ef0e058cf8e9
Siwei Hu <hu.1985972@studenti.uniroma1.it>
2024-06-20 16:54:04 +0200

    added new gestitue to start new game

--- src/main.py
+++ src/main.py
@@ -223,6 +223,9 @@ def isOccupied(grid, index_pos):
         return True
 
 
+    
+
+
 menu = True
 running = True
 show_text = True
@@ -320,6 +323,17 @@ while running:
 
             else:
                 Erasing = False
+            
+            if fingers == [True, False, False, False, True] and match_done:
+                match_done = False
+                draws = [[]]
+                drawNumber = 0
+                # startCell = None
+                grid_array = [["", "", ""], ["", "", ""], ["", "", ""]]
+                puntO = 0
+                puntX = 0
+
+
 
     if menu:
         WIN.blit(background_image, (0, 0))
